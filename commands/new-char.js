0b0178214afed40f436dc695e898ad447ea2d4fc
pcmasonjar <35505948+pcmasonjar@users.noreply.github.com>
2018-01-23 00:45:39 -0500

    new char update

--- commands/new-char.js
+++ commands/new-char.js
@@ -5,6 +5,13 @@ exports.run = (client, message, args) => {
     let race = args[1];
     let gender = args[2];
     let gm = require(`../GameManager.js`);
+    if (race.toLowerCase() != "dwarf" && race.toLowerCase() != "elf" && race.toLowerCase() != "human" && race.toLowerCase() != "orc"){
+        message.channel.send('Please choose one of the following race: Dwarf, Elf, Human, or Orc')
+    }
+    if (gender.toLowerCase() != "male" && gender.toLowerCase() != "female"){
+        message.channel.send('Please choose one of the following genders: Female or Male')
+        return
+    }
     gm.AddChar(name, race, gender);
     message.channel.send(`Created new character: ${name} ${race} ${gender}`).catch(console.error);
 }
